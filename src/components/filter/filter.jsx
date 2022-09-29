1c66aa6d2f26a2dce49f99369ddbd6b4a253ba64
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-29 22:16:53 +0300

    Create filter.jsx

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,24 @@
+import { filterContact } from '../../redux/actions.js';
+// import { filterContact } from '../../redux/contactSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import css from './filter.module.css';
+
+export function Filter() {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contactsReducer.filter);
+
+  const changeFilter = data => {
+    dispatch(filterContact(data));
+  };
+  return (
+    <label className={css.label}>
+      <input
+        className={css.input}
+        type="text"
+        placeholder="Find contacts by name"
+        value={filter}
+        onChange={e => changeFilter(e.target.value)}
+      />
+    </label>
+  );
+}
