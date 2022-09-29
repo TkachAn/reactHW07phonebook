30b0a45ab3de7e41a18bc076e83c58900b773395
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-29 22:15:53 +0300

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './reducer.js';
+// import { contactsReducer } from './contactSlice';
+export const store = configureStore({
+  reducer: {
+    contactsReducer,
+  },
+});
