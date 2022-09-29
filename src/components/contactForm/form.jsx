22d357fb90937698c209247d01e01c37c0ec1235
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-29 22:17:20 +0300

    Create form.jsx

---
+++ src/components/contactForm/form.jsx
@@ -0,0 +1,57 @@
+import { nanoid } from 'nanoid';
+import { addContact } from '../../redux/actions';
+// import { addContact } from '../../redux/contactSlice';
+import { useDispatch } from 'react-redux';
+import css from './form.module.css';
+
+export function ContactForm() {
+  const dispatch = useDispatch();
+
+  /** */
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+    console.log(name, number);
+    form.reset();
+    const data = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    dispatch(addContact(data));
+  };
+  /** */
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          placeholder="enter your name, please."
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          placeholder="enter your phone number, please."
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
