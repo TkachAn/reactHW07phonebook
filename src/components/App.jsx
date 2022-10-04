f36521a57b46e51d766744e8579010e198844b04
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-04 19:14:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,17 @@ import { useSelector } from 'react-redux';
 
 import css from './App.module.css';
 export const App = () => {
-  const contacts = useSelector(state => state.contactsReducer).items;
+  const contacts = useSelector(state => state.contactsReducer);
 
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {Number(contacts.length) > 1 && <Filter />}
-      {contacts.items.length > 0 ? (
+      {/* {contacts.items.length > 1 && <Filter />} */}
+      {contacts.length > 1 && <Filter />}
+      {/* {contacts.items.length > 0 ? ( */}
+      {contacts.length > 0 ? (
         <ContactList />
       ) : (
         <p>Your phonebook is empty. Please add contact.</p>
@@ -21,3 +23,28 @@ export const App = () => {
     </div>
   );
 };
+
+// import { Filter } from './filter/filter';
+// import { ContactForm } from './contactForm/form';
+// import { ContactList } from './contactList/list';
+// import { useSelector } from 'react-redux';
+
+// import css from './App.module.css';
+// export const App = () => {
+//   const contacts = useSelector(state => state.contactsReducer);
+
+//   console.log('contacts.length', contacts.length);
+//   return (
+//     <div className={css.container}>
+//       <h1>Phonebook</h1>
+//       <ContactForm />
+//       <h2>Contacts</h2>
+//       {Number(contacts.length) > 1 && <Filter />}
+//       {contacts.length > 0 ? (
+//         <ContactList />
+//       ) : (
+//         <p>Your phonebook is empty. Please add contact.</p>
+//       )}
+//     </div>
+//   );
+// };
