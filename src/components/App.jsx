98d4a0ad53c1dcc12fdaaaf0756c1eed357c7f78
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-04 13:22:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,14 @@ import { useSelector } from 'react-redux';
 
 import css from './App.module.css';
 export const App = () => {
-  const contacts = useSelector(state => state.contactsReducer);
+  const contacts = useSelector(state => state.contactsReducer).items;
 
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {contacts.items.length > 1 && <Filter />}
+      {Number(contacts.length) > 1 && <Filter />}
       {/* <Filter /> */}
       {/* <ContactList /> */}
       {contacts.items.length > 0 ? (
