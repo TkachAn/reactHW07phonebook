0adae0fdb02aaa8dfe61d40512f1387ec98edd12
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 23:54:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,32 @@
-// import { Filter } from './filter/filter';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactForm } from './contactForm/form';
 import { ContactList } from './contactList/list';
-// import { getContact } from 'redux/selectors';
-// import { getContacts } from 'redux/selectors';
-import { fetchContacts } from 'redux/operAxios';
-import { useDispatch, useSelector } from 'react-redux'; //
+import { Filter } from './filter/filter';
+import { selectContacts } from 'redux/selectors';
 import { useEffect } from 'react';
-// import { getError, getIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+import css from './App.module.css';
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const isLoading = useSelector(getIsLoading);
-  // const error = useSelector(getError);
-  const { items, isLoading, error } = useSelector(fetchContacts);
-  console.log('App-getContacts', useSelector(fetchContacts));
-  console.log('App-items', { items });
+  const { contacts, isLoading } = useSelector(selectContacts);
 
-  // const { items, isLoading, error } = useSelector(getContacts);
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
-    <div>
-      {/* <AppBar /> */}
-      <ContactForm />
-      {/* {isLoading && !error && <b>Request in progress...</b>} */}
-      <ContactList />
-      {/* {isLoading && <p>Loading tasks...</p>}
-      {error && <p>{error}</p>} */}
-      {/* <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p> */}
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} />
+      <h2>Contacts</h2>
+      {Number(contacts.length) > 1 && <Filter />}
+      {isLoading && <p>Loading contacts...</p>}
+      {contacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        <p>Your phonebook is empty. Please add contact.</p>
+      )}
     </div>
   );
 };
-
-// import css from './App.module.css';
-// export const App = () => {
-//   const contacts = []; //useSelector(state => state.contactsReducer);
-
-//   return (
-//     <div className={css.container}>
-//       <h1>Phonebook</h1>
-//       <ContactForm />
-//       <h2>Contacts</h2>
-//       {contacts.items.length > 1 && <Filter />}
-//       {/* {contacts.length > 1 && <Filter />} */}
-//       {contacts.items.length > 0 ? (
-//         <ContactList />
-//       ) : (
-//         <p>Your phonebook is empty. Please add contact.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// import { Filter } from './filter/filter';
-// import { ContactForm } from './contactForm/form';
-// import { ContactList } from './contactList/list';
-// import { useSelector } from 'react-redux';
-
-// import css from './App.module.css';
-// export const App = () => {
-//   const contacts = useSelector(state => state.contactsReducer);
-
-//   console.log('contacts.length', contacts.length);
-//   return (
-//     <div className={css.container}>
-//       <h1>Phonebook</h1>
-//       <ContactForm />
-//       <h2>Contacts</h2>
-//       {Number(contacts.length) > 1 && <Filter />}
-//       {contacts.length > 0 ? (
-//         <ContactList />
-//       ) : (
-//         <p>Your phonebook is empty. Please add contact.</p>
-//       )}
-//     </div>
-//   );
-// };
