24b45db86e2101cd9d3f25847aabafd613f3d073
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 08:51:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,48 @@
-import { Filter } from './filter/filter';
-import { ContactForm } from './contactForm/form';
-import { ContactList } from './contactList/list';
-import { useSelector } from 'react-redux';
+// import { Filter } from './filter/filter';
+// import { ContactForm } from './contactForm/form';
+// import { ContactList } from './contactList/list';
+import { getContacts } from 'redux/selectors';
+import { fetchContacts } from 'redux/operAxios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-import css from './App.module.css';
 export const App = () => {
-  const contacts = useSelector(state => state.contactsReducer);
+  const dispatch = useDispatch();
+
+  const { items, isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      {contacts.items.length > 1 && <Filter />}
-      {/* {contacts.length > 1 && <Filter />} */}
-      {contacts.items.length > 0 ? (
-        <ContactList />
-      ) : (
-        <p>Your phonebook is empty. Please add contact.</p>
-      )}
+    <div>
+      {isLoading && <p>Loading tasks...</p>}
+      {error && <p>{error}</p>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
     </div>
   );
 };
+
+// import css from './App.module.css';
+// export const App = () => {
+//   const contacts = []; //useSelector(state => state.contactsReducer);
+
+//   return (
+//     <div className={css.container}>
+//       <h1>Phonebook</h1>
+//       <ContactForm />
+//       <h2>Contacts</h2>
+//       {contacts.items.length > 1 && <Filter />}
+//       {/* {contacts.length > 1 && <Filter />} */}
+//       {contacts.items.length > 0 ? (
+//         <ContactList />
+//       ) : (
+//         <p>Your phonebook is empty. Please add contact.</p>
+//       )}
+//     </div>
+//   );
+// };
 
 // import { Filter } from './filter/filter';
 // import { ContactForm } from './contactForm/form';
