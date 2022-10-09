2083aef6aa853e99b85cbdb653d344acfae7cfab
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 08:51:29 +0300

    Update filter.jsx

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,28 +1,28 @@
-import css from './filter.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { actions } from '../../redux/actions';
-import { selectors } from '../../redux/selectors';
-import { filterContact } from '../../redux/actions.js';
-// import { filterContact } from '../../redux/contactSlice';
+// import css from './filter.module.css';
+// // import { useSelector, useDispatch } from 'react-redux';
+// // import { actions } from '../../redux/actions';
+// // import { selectors } from '../../redux/selectors';
+// // import { filterContact } from '../../redux/actions.js';
+// // import { filterContact } from '../../redux/contactSlice';
 
-export function Filter() {
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.contactsReducer.filter);
-  // const filter = useSelector(state => state.contactsReducer.filter);
-  console.log('filter', filter);
-  const changeFilter = data => {
-    console.log('filterContact(data)', filterContact(data));
-    dispatch(filterContact(data));
-  };
-  return (
-    <label className={css.label}>
-      <input
-        className={css.input}
-        type="text"
-        placeholder="Find contacts by name"
-        value={filter}
-        onChange={e => changeFilter(e.target.value)}
-      />
-    </label>
-  );
-}
+// export function Filter() {
+//   // const dispatch = useDispatch();
+//   const filter = ''; //useSelector(state => state.contactsReducer.filter);
+//   // const filter = useSelector(state => state.contactsReducer.filter);
+//   // console.log('filter', filter);
+//   const changeFilter = data => {
+//     // console.log('filterContact(data)', filterContact(data));
+//     // dispatch(filterContact(data));
+//   };
+//   return (
+//     <label className={css.label}>
+//       <input
+//         className={css.input}
+//         type="text"
+//         placeholder="Find contacts by name"
+//         value={filter}
+//         onChange={e => changeFilter(e.target.value)}
+//       />
+//     </label>
+//   );
+// }
