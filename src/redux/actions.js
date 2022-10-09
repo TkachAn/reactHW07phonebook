978aa97c8ade48a735a97b50e02e7e4fe2f723a2
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 23:54:02 +0300

    Delete actions.js

--- src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-// import { createAction } from '@reduxjs/toolkit';
-
-// // export const changeFilter = createAction('phonebook/filterChange');
-// // export const filterContact = createAction('contact/filter'); //
-// const addContact = createAction('contacts/addContact');
-// const deleteContact = createAction('contacts/deleteContact');
-// const filterContact = createAction('contacts/filterContact');
-
-// export { addContact, deleteContact, filterContact };
