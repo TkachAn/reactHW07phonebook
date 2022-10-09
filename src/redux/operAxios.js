80edca63503d3e2582a1135003cdc0bed9d84567
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 17:03:43 +0300

    Update operAxios.js

--- src/redux/operAxios.js
+++ src/redux/operAxios.js
@@ -2,17 +2,47 @@ import axios from 'axios';
 // import { useDispatch } from 'react-redux';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// import {
-//   fetchingInProgress,
-//   fetchingSuccess,
-//   fetchingError,
-// } from './contactSlice';
+// axios.defaults.baseURL = 'https://633ef5ed0dbc3309f3c20373.mockapi.io';//6335c6d265d1e8ef2663035d
+axios.defaults.baseURL = 'https://6335c6d265d1e8ef2663035d.mockapi.io';
+export const deleteContact = createAsyncThunk(
+  'contact/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/tasks/${contactId}`);
+      console.log('Del-response', response.data);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
 
-axios.defaults.baseURL = 'https://633ef5ed0dbc3309f3c20373.mockapi.io';
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, phone }, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { name, phone });
+      console.log('Add-response', response.data);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
-  // () => async dispatch => {
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      // console.log('All-response', response.data);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+  //() =>
+  // async dispatch => {
   //   try {
   //     dispatch(fetchingInProgress());
   //     const response = await axios.get('/contacts');
@@ -20,13 +50,6 @@ export const fetchContacts = createAsyncThunk(
   //   } catch (e) {
   //     dispatch(fetchingError(e.message));
   //   }
-  // },() =>
-  async (_, thunkAPI) => {
-    try {
-      const response = await axios.get('/contacts');
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
+  // }
+  // //,() =>
 );
