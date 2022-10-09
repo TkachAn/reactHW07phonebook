e73ca8e89394cdef29b265d048a8087d4503ed7d
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 17:03:51 +0300

    Update contactsAPI.js

--- src/redux/contactsAPI.js
+++ src/redux/contactsAPI.js
@@ -1,38 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const contactsApi = createApi({
-  reducerPath: 'contacts',
+// export const contactsApi = createApi({
+//   reducerPath: 'contacts',
 
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://633ef5ed0dbc3309f3c20373.mockapi.io',
-  }),
-  tagTypes: ['Contact'],
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: 'https://633ef5ed0dbc3309f3c20373.mockapi.io',
+//   }),
+//   tagTypes: ['Contact'],
 
-  endpoints: builder => ({
-    getContacts: builder.query({
-      query: () => '/contacts',
-      providesTags: ['Contact'],
-    }),
-    addContact: builder.mutation({
-      query: data => ({
-        url: `/contacts`,
-        method: 'POST',
-        body: data,
-      }),
-      invalidatesTags: ['Contact'],
-    }),
-    deleteContact: builder.mutation({
-      query: id => ({
-        url: `/contacts/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Contact'],
-    }),
-  }),
-});
+//   endpoints: builder => ({
+//     getContacts: builder.query({
+//       query: () => '/contacts',
+//       providesTags: ['Contact'],
+//     }),
+//     addContact: builder.mutation({
+//       query: data => ({
+//         url: `/contacts`,
+//         method: 'POST',
+//         body: data,
+//       }),
+//       invalidatesTags: ['Contact'],
+//     }),
+//     deleteContact: builder.mutation({
+//       query: id => ({
+//         url: `/contacts/${id}`,
+//         method: 'DELETE',
+//       }),
+//       invalidatesTags: ['Contact'],
+//     }),
+//   }),
+// });
 
-export const {
-  useGetContactsQuery,
-  useAddContactMutation,
-  useDeleteContactMutation,
-} = contactsApi;
+// export const {
+//   useGetContactsQuery,
+//   useAddContactMutation,
+//   useDeleteContactMutation,
+// } = contactsApi;
