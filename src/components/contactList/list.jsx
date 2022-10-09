a6ba4782bd94649d2e8ab44f7995069a681497c7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 17:04:00 +0300

    Update list.jsx

--- src/components/contactList/list.jsx
+++ src/components/contactList/list.jsx
@@ -1,5 +1,9 @@
-// import { useDispatch, useSelector } from 'react-redux';
-
+import { useSelector } from 'react-redux'; // useDispatch,
+import { getContacts } from 'redux/selectors';
+// import { getContacts } from 'redux/selectors';
+import { fetchContacts } from 'redux/operAxios';
+// import { fetchContacts } from 'redux/operAxios';
+// import { deleteContact } from 'redux/operAxios';
 // import { deleteContact } from '../../redux/actions.js';
 // import { deleteContact } from '../../redux/contactSlice';
 // import { getList } from '../../redux/selectors';
@@ -7,30 +11,40 @@ import css from './list.module.css';
 
 export function ContactList() {
   // const dispatch = useDispatch();
-  const contacts = []; //useSelector(getList);
+  const contacts = [{}, {}]; //useSelector(fetchContacts);
+  const { items } = useSelector(fetchContacts);
+  console.log('List-getContacts', useSelector(getContacts));
+  console.log('List-items', { items });
+  // const contacts = fetchContacts();
+  // const contacts = useSelector(fetchContacts());
+  // const contacts = useSelector(getContacts);
+  // const contacts = useSelector(state => state.contacts);
+  // const contacts = useSelector(getContacts.item);
   // console.log('getList', getList);
-  // console.log('contacts', contacts);
-  const delContacts = data => {
-    // console.log(deleteContact(data));
-    // dispatch(deleteContact(data));
-  };
+  console.log('List--contacts', contacts); //contacts
+  // const delContacts = data => {
+  //   // console.log(deleteContact(data));
+  //   // dispatch(deleteContact(data));
+  // };
 
   return (
     <ul className={css.list}>
-      {contacts.map(({ id, name, number }) => (
-        <li className={css.item} key={id}>
-          <div className={css.info}>
-            {name}: {number}
-            <button
-              className={css.btn}
-              type="button"
-              onClick={() => delContacts(id)}
-            >
-              X
-            </button>
-          </div>
-        </li>
-      ))}
+      {contacts.map(({ id, name, phone }) => {
+        return (
+          <li className={css.item} key={id}>
+            <div className={css.info}>
+              {name}: {phone}
+              <button
+                className={css.btn}
+                type="button"
+                // onClick={() => delContacts(id)}
+              >
+                X
+              </button>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
