2e40f61e3ff47bbe7bc6d6139d3c8caa32bd57ec
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 08:51:10 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,40 +1,41 @@
-import { createReducer } from '@reduxjs/toolkit';
-import { addContact, deleteContact, filterContact } from './actions.js';
+// import { createReducer } from '@reduxjs/toolkit';
+// import { addContact, deleteContact, filterContact } from './actions';
 
-import {
-  loadFromStorage,
-  saveInStorage,
-} from '../storageService/storageService';
+// import {
+//   loadFromStorage,
+//   saveInStorage,
+// } from '../storageService/storageService';
 
-export const contactsReducer = createReducer(
-  {
-    items: loadFromStorage('phonebook'),
-    filter: '',
-    isLoading: false,
-    error: null,
-  },
-  {
-    [addContact]: (state, action) => {
-      console.log('state', state);
-      const isNameExist = state.items.find(
-        contact =>
-          contact.name.toLowerCase() === action.payload.name.toLowerCase()
-      );
+// export const contactsReducer = createReducer(
+//   {
+//     items: loadFromStorage('phonebook'),
 
-      console.log('isNameExist', isNameExist);
-      isNameExist
-        ? alert(`${action.payload.name} is already in contacts`)
-        : state.items.push(action.payload);
-      saveInStorage('phonebook', state.items);
-    },
-    [deleteContact]: (state, action) => {
-      state.items = state.items.filter(
-        contact => contact.id !== action.payload
-      );
-      saveInStorage('phonebook', state.items);
-    },
-    [filterContact]: (state, action) => {
-      state.filter = action.payload.toLowerCase();
-    },
-  }
-);
+//     isLoading: false,
+//     error: null,
+//   },
+//   {
+//     [addContact]: (state, action) => {
+//       console.log('state', state);
+//       const isNameExist = state.items.find(
+//         contact =>
+//           contact.name.toLowerCase() === action.payload.name.toLowerCase()
+//       );
+
+//       console.log('isNameExist', isNameExist);
+//       isNameExist
+//         ? alert(`${action.payload.name} is already in contacts`)
+//         : state.items.push(action.payload);
+//       saveInStorage('phonebook', state.items);
+//     },
+//     [deleteContact]: (state, action) => {
+//       state.items = state.items.filter(
+//         contact => contact.id !== action.payload
+//       );
+//       saveInStorage('phonebook', state.items);
+//     },
+//     // [filterContact]: (state, action) => {
+//     //   state.filter = action.payload.toLowerCase(); //.toLowerCase()
+//     // },
+//   }
+// );
+// // export {addContact, deleteContact, filterContact} = contactsReducer;
