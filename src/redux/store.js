384d1d326e6a9ccb902eca09b66cd4657b316f3b
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-09 17:03:35 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactSlice';
 // import { contactsReducer } from './contactSlice';
+console.log('Store-contactsReducer', contactsReducer);
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
